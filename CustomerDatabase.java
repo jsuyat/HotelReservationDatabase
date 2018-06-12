bc8bf93cec4b3864a24659eddf8688d291ac0559
jsuyat <jsuyat@masonlive.gmu.edu>
2018-06-12 14:24:50 -0400

    Update CustomerDatabase.java

--- CustomerDatabase.java
+++ CustomerDatabase.java
@@ -16,8 +16,8 @@ public class CustomerDatabase{
   private String jdbc_url = "jdbc:oracle:thin:@apollo.vse.gmu.edu:1521:ite10g";
 
   //login information
-  private String username = "jsuyat";
-  private String password = "gubohu";
+  private String username = "***";
+  private String password = "***";
 
   //Default constructor
   public CustomerDatabase(){}
